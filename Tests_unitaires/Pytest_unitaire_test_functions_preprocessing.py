cc3eb13c43aa7c3afdd18cd3505f45c1af234c3e
Nadiasami <153780903+Nadiasami@users.noreply.github.com>
2024-01-13 01:03:23 +0100

    Ajout_unitaire_test_functions_preprocessing.py

--- Tests_unitaires/Pytest_unitaire_test_functions_preprocessing.py
+++ Tests_unitaires/Pytest_unitaire_test_functions_preprocessing.py
@@ -11,13 +11,13 @@ from Script import cleaning_feature_engineering
 from Script.cleaning_feature_engineering import remplir_valeurs_manquantes, supprimer_colonnes_manquantes, supprimer_var_correl, supprimer_colonnes_valeurs_uniques, one_hot_encoder
 
 # Obtenez le chemin absolu du fichier CSV
-csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Feature_importance_for_model_optimi_time', 'feature_importance_df.csv'))
+
 
 ########################## Test function lire_et_traiter_donnees #############################################################################################################################
 
 def test_csv_loading():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join(current_directory, "..", "Feature_importance_for_model_optimi_time", "feature_importance_df.csv")
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Feature_importance_for_model_optimi_time', 'feature_importance_df.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
     df = pd.read_csv(csv_path)
     # Appliquer les lignes de code à tester
@@ -69,8 +69,8 @@ def test_supprimer_colonnes_valeurs_uniques():
 
 def test_application_train_test():
     # Détermine le chemin du fichier CSV
-    csv_path_1 = os.path.join(current_directory, "..", "..", "data", "application_train.csv")
-    csv_path_2 = os.path.join(current_directory, "..", "..", "data", "application_test.csv")
+    csv_path_1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'application_train.csv'))
+    csv_path_2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'application_test.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
     df1= pd.read_csv(csv_path_1)
     df2= pd.read_csv(csv_path_2) 
@@ -82,8 +82,8 @@ def test_application_train_test():
 
 def test_bureau_and_balance():
     # Détermine le chemin du fichier CSV
-    csv_path_1 = os.path.join(current_directory, "..", "..", "data", "bureau.csv")
-    csv_path_2 = os.path.join(current_directory, "..", "..", "data", "bureau_balance.csv")
+    csv_path_1 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'bureau.csv'))
+    csv_path_2 = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'bureau_balance.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
     df1= pd.read_csv(csv_path_1)
     df2= pd.read_csv(csv_path_2) 
@@ -95,37 +95,47 @@ def test_bureau_and_balance():
 
 def test_previous_applications():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join(current_directory, "..", "..", "data", "previous_application.csv")
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'previous_application.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
-    df1= pd.read_csv(csv_path) 
+    df= pd.read_csv(csv_path) 
     # Vérifie que le DataFrame n'est pas vide
-    assert not df1.empty, "Erreur dans le chargement du CSV previous_application."
+    assert not df.empty, "Erreur dans le chargement du CSV previous_application."
     
 ########################## Test function POS_CASH_balance #############################################################################################################################
 
 def test_POS_CASH_balance():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join(current_directory, "..", "..", "data", "POS_CASH_balance.csv")
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'POS_CASH_balance.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
-    df1= pd.read_csv(csv_path) 
+    df= pd.read_csv(csv_path) 
     # Vérifie que le DataFrame n'est pas vide
-    assert not df1.empty, "Erreur dans le chargement du CSV POS_CASH_balance."
+    assert not df.empty, "Erreur dans le chargement du CSV POS_CASH_balance."
     
 ########################## Test function installments_payments #############################################################################################################################
 
 def test_installments_payments():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join(current_directory, "..", "..", "data", "installments_payments.csv")
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'installments_payments.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
-    df1= pd.read_csv(csv_path) 
+    df= pd.read_csv(csv_path) 
     # Vérifie que le DataFrame n'est pas vide
-    assert not df1.empty, "Erreur dans le chargement du installments_payments."
+    assert not df.empty, "Erreur dans le chargement du installments_payments."
+
+########################## Test function credit_card_balance #############################################################################################################################
+
+def test_icredit_card_balance():
+    # Détermine le chemin du fichier CSV
+    csv_path = os.path.join(current_directory, "..", "data", "credit_card_balance.csv")
+    # Charge le fichier CSV dans un DataFrame pandas
+    df= pd.read_csv(csv_path) 
+    # Vérifie que le DataFrame n'est pas vide
+    assert not df.empty, "Erreur dans le chargement du credit_card_balance."
     
 ########################## Test function credit_card_balance #############################################################################################################################
 
 def test_icredit_card_balance():
     # Détermine le chemin du fichier CSV
-    csv_path = os.path.join(current_directory, "..", "..", "data", "credit_card_balance.csv")
+    csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', 'credit_card_balance.csv'))
     # Charge le fichier CSV dans un DataFrame pandas
     df1= pd.read_csv(csv_path) 
     # Vérifie que le DataFrame n'est pas vide
